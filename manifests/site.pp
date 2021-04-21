d1cfcf3a52a006080d4caa64972860ef6c0cfeab
Ivan Roseland <icroseland@gmail.com>
2021-04-21 13:51:41 -0500

    goign old school.. for a moment

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,6 @@
 # Puppet Managed
 # we will be using include classes to drive roles directly from hiera along with hosts facts.
 # 
-node default {
-  # Find the first instance of `classes` in hiera data and includes unique values. Does not merge results.
-  $classes = lookup('classes', Variant[String,Array[String]])
-  case $classes {
-    String[1]: {
-      include $classes
-    }
-    Array[String[1],1]: {
-      $classes.unique.include
-    }
-    default: {
-      fail('This node did not receive any classification')
-    }
-  }
+node defualt {
+hiera_include('classes')
 }
