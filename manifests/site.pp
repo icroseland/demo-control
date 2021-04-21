0d00dad8f55ed1d94515d7048c9f9f847de26181
Ivan Roseland <icroseland@gmail.com>
2021-04-21 12:33:21 -0500

    new way to include classes, better?

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,18 @@
 # Puppet Managed
 # we will be using include classes to drive roles directly from hiera along with hosts facts.
 # 
-hiera_include('classes')
+node default {
+  # Find the first instance of `classes` in hiera data and includes unique values. Does not merge results.
+  $classes = lookup('classes', Variant[String,Array[String]])
+  case $classes {
+    String[1]: {
+      include $classes
+    }
+    Array[String[1],1]: {
+      $classes.unique.include
+    }
+    default: {
+      fail('This node did not receive any classification')
+    }
+  }
+}
